9ae6ce9c7887637ea702494a6da5dca4469bd28d
bridge <kingbridgess@sina.com>
2023-09-13 14:12:11 +0800

    tree differece error handling

--- src/main/java/org/bridge/filetree/TreeDifference.java
+++ src/main/java/org/bridge/filetree/TreeDifference.java
@@ -6,6 +6,9 @@ public class TreeDifference {
     private TreeNode node2;
 
     public TreeDifference(TreeNode node1, TreeNode node2) {
+        if (!((node1 == null) ^ (node2 == null))) {
+            throw new IllegalArgumentException("node1 and node2 should have and only have one null value.");
+        }
         this.node1 = node1;
         this.node2 = node2;
     }
--- src/main/java/org/bridge/filetree/TreeNode.java
+++ src/main/java/org/bridge/filetree/TreeNode.java
@@ -7,28 +7,16 @@ public class TreeNode {
     private String name;
     private List<TreeNode> children;
     private String path;
-
     public TreeNode(String name) {
         this.name = name;
         this.children = new ArrayList<>();
     }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public List<TreeNode> getChildren() {
         return children;
     }
-
-    public void setChildren(List<TreeNode> children) {
-        this.children = children;
-    }
-
     public String getPath() {
         return path;
     }
@@ -36,7 +24,6 @@ public class TreeNode {
     public void setPath(String path) {
         this.path = path;
     }
-
     public void addChild(TreeNode child) {
         children.add(child);
     }
